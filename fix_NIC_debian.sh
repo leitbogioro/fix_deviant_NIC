e50b225ffc96a54b63f14ddc332b4e359d5fc147
Molly Lau <windowsbeta@outlook.com>
2018-12-23 15:36:33 +0800

    Add files via upload

---
+++ fix_NIC_debian.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          molly lau
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: fix deviant NIC
+# Description:       fix from deviant NIC deamon
+### END INIT INFO
+
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# Colors
+green='\033[0;32m'
+red='\033[0;31m'
+plain='\033[0m'
+
+# CheckRoot 
+[ $(id -u) != "0" ] && { echo -e "[${red}Error${plain}] You must run me as a root! "; exit 1; }
+
+# Disable SELinux
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+
+CurrentETH=`ifconfig -a | awk '{print $1}' | head -n 1`
+ConfigETH=`cat /etc/network/interfaces | sed -n '$p'  | awk '{print $2}'`
+Interface="/etc/network/interfaces"
+
+# Check ping
+IPLists="1.1.1.1 8.8.8.8 9.9.9.9"
+result=0
+PingStatus() {
+    if ping -c 1 $IP > /dev/null; then
+        result=`expr $result + 1`
+        continue
+    fi
+    return $result
+}
+StartPing() {    
+    for IP in $IPLists; do
+        PingStatus
+    done
+}
+StartPing
+
+# WriteNIC
+configETH() {
+    rm -rf ${Interface}
+    cat > ${Interface}<<-EOF
+auto lo
+iface lo inet loopback
+auto ${CurrentETH}
+iface ${CurrentETH} inet dhcp
+EOF
+    service networking restart
+}
+
+if [[ ! ${CurrentETH} == ${ConfigETH} ]] && [[ ! ${result} -lt 0 ]]; then
+    configETH
+    echo -e "[${green}Congratulations${plain}] Your fuckin network will be okay! "
+    rm -rf Deviant_NIC.sh
+else
+    echo -e "[${green}Congratulations${plain}] Your network seems have no problem, No alternations need to be applied! "
+    rm -rf Deviant_NIC.sh
+    exit 1
+fi
---
+++ fix_deviant_NIC.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# Colors
+green='\033[0;32m'
+red='\033[0;31m'
+plain='\033[0m'
+
+d_file="/etc/init.d/fix_NIC_debian.sh"
+d_file_url=""
+c_file="/etc/rc.d/init.d/fix_NIC_redhat.sh"
+c_file_url=""
+hint="Install successfully, it can prevent you from losing network connection after recovery from snapshot! "
+
+# CheckRoot
+[ $(id -u) != "0" ] && { echo -e "[${red}Error${plain}] You must run me as a root! "; exit 1; }
+
+# Judge OS
+if [ -f /etc/redhat-release ];then
+        OS='CentOS'
+    elif [ ! -z "`cat /etc/issue | grep -i 'bian'`" ];then
+        OS='Debian'
+    elif [ ! -z "`cat /etc/issue | grep -i 'buntu'`" ];then
+        OS='Ubuntu'
+    else
+        echo -e "We do not support current system, fix will exit!"
+        exit 1
+fi
+
+# Disable SELinux
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+
+Write_apt_task() {
+    if ! wget ${d_file_url} -O ${d_file}; then
+        echo -e "[${red}Error${plain}] Downloading ${d_file} failed!"
+        exit 1
+    fi
+    chmod 775 ${d_file}
+    update-rc.d ${d_file} defaults 20
+}
+
+Write_yum_tast() {
+    if ! wget ${c_file_url} -O ${c_file}; then
+        echo -e "[${red}Error${plain}] Downloading ${c_file} failed!"
+        exit 1
+    fi
+    chmod 775 ${c_file}
+    chkconfig --add ${c_file}
+    chkconfig ${c_file} on
+}
+
+if [[ ${OS} == 'Debian' ]] || [[ ${OS} == 'Ubuntu' ]]; then
+    Write_apt_task
+    echo -e "[${red}ok${plain}] ${hint}"
+    rm $0
+elif [[ ${OS} == 'CentOS' ]]; then
+    Write_yum_task
+    echo -e "[${red}ok${plain}] ${hint}"
+    rm $0
+fi
